1829697acb2d65a8c65dbadab3079fdf10d5fd31
胖衛 <58324866+lidawei97688@users.noreply.github.com>
2025-11-06 15:53:12 +0800

    更新 action.sh
    在三个路径里查找openlist二进制文件路径，把路径结果告知pgrep命令并执行

--- OpenList-Magisk/action.sh
+++ OpenList-Magisk/action.sh
@@ -5,16 +5,15 @@
 MODDIR="${0%/*}"
 MODULE_PROP="$MODDIR/module.prop"
 SERVICE_SH="$MODDIR/service.sh"
+OPLISTDIR="/data/adb/openlist/bin" "$MODDIR/bin" "/system/bin"
 REPO_URL="https://github.com/Alien-Et/OpenList-Magisk"
 
 check_openlist_status() {
-    for bf in /data/adb/openlist/bin/openlist \
-              $MODDIR/bin/openlist \
-              /system/bin/openlist
-    do
-        pgrep -f "${bf} server" >/dev/null && return 0
-    done
-    return 1
+ if result=$(find $OPLISTDIR -name "openlist" 2>/dev/null); then
+ pgrep -f "$result server" >/dev/null && return 0;
+	else
+	return 1
+	fi
 }
 
 update_module_prop_stopped() {
